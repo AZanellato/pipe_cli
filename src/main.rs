d3b55a4f7ff415b177bb29d61ed08d925f818cfe
Andre Zanellato <andre.zanellato@gmail.com>
2019-06-14 02:16:43 -0300

    Add some menus

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate dialoguer;
+extern crate indicatif;
+extern crate itertools;
 extern crate serde;
 extern crate serde_json;
 extern crate structopt;
@@ -6,7 +9,11 @@ mod args;
 mod graphql;
 use api_key::ApiKey;
 use confy::{load, store};
+use dialoguer::{theme::ColorfulTheme, Input, Select};
+use indicatif::ProgressBar;
+use itertools::any;
 use quicli::prelude::*;
+use std::borrow::Cow;
 use structopt::StructOpt;
 
 fn main() -> CliResult {
@@ -15,8 +22,70 @@ fn main() -> CliResult {
         Some(key) => api_key::test_existing_api_key(key),
         None => api_key::get_working_api_key(),
     };
-    store("pipe_cli", working_cfg.clone())?;
     println!("Hello {}! Welcome to Pipefy CLI", working_cfg.name);
+    store("pipe_cli", &working_cfg)?;
     let args = args::Opts::from_args();
+    let no_selection = !any(&[args.pipe_id, args.card_id], |id| id.is_some());
+
+    if no_selection {
+        let (selected_option, inputed_id) = main_select();
+        // println!("option {} and id {}", selected_option, inputed_id);
+        match selected_option {
+            0 => pipe_sub_select(),
+            1 => org_sub_select(),
+            2 => card_sub_select(&working_cfg.api_key.unwrap(), inputed_id),
+            _ => main_select(),
+        };
+    }
+    ProgressBar::new_spinner();
     Ok(())
+}
+
+fn main_select<'a>() -> (i32, i32) {
+    let selections = &["💈 Pipe", "🏭 Company", "🃏 Card"];
+
+    let select = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Choose what you want to see")
+        .default(0)
+        .items(&selections[..])
+        .interact()
+        .unwrap();
+
+    let input: i32 = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt("The ID, please")
+        .interact()
+        .unwrap();
+
+    (select as i32, input)
+}
+
+fn pipe_sub_select<'a>() -> (i32, i32) {
+    let selections = &["Phases", "Cards"];
+
+    let select = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("What do you want to see?")
+        .default(0)
+        .items(&selections[..])
+        .interact()
+        .unwrap();
+
+    (select as i32, 0)
+}
+fn card_sub_select(api_key: &str, id: i32) -> (i32, i32) {
+    let card_title = graphql::card_query(api_key, id);
+    println!("{}", card_title.unwrap());
+
+    (0, 0)
+}
+fn org_sub_select<'a>() -> (i32, i32) {
+    let selections = &["💈 Pipe", "🏭 Company", "🃏 Card"];
+
+    let select = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("What do you want to see?")
+        .default(0)
+        .items(&selections[..])
+        .interact()
+        .unwrap();
+
+    (select as i32, 0)
 }
